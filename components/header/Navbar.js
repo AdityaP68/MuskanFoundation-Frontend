d84bede1ebe1628d6bdfdabe2f3ed374bedf13c8
Aditya Prakash <adityaprakash6688@gmail.com>
2022-10-20 22:57:54 +0530

    links added 1

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,5 +1,6 @@
 import styles from "./Navbar.module.scss";
 import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
 
 function Navbar() {
   return (
@@ -8,18 +9,26 @@ function Navbar() {
         <span>012-3846579 | Muskan@foundation.org</span>
       </address>
       <nav>
-        <div className={styles.logo}>
-          <img src={"/images/logo.png"} />
-        </div>
+        <Link href="/">
+          <a>
+            <div className={styles.logo}>
+              <img src={"/images/logo.png"} />
+            </div>
+          </a>
+        </Link>
         <ul className={styles.navContainer}>
-          <li>About Us</li>
+          <Link href="/campaigns">
+            <a>
+              <li>About Us</li>
+            </a>
+          </Link>
           <li>Campaign</li>
           <li>Gallery</li>
           <div className={styles.contriBtn}>Contribute</div>
           <li>Contact Us</li>
         </ul>
         <span
-            className = {styles.hamburgerMenu}
+          className={styles.hamburgerMenu}
           onClick={() => {
             console.log("click");
           }}
--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Navbar from '../../components/header/Navbar'
+import React from "react";
+import Banner from "../../components/campaigns/banner/Banner";
+import Navbar from "../../components/header/Navbar";
+import styles from "./Campaigns.module.scss";
 
 function index() {
   return (
     <React.Fragment>
-        <Navbar/>
-        <main>
-        
-        </main>
+      <Navbar />
+      <main>
+        <Banner />
+        <div className={styles.mainContent}>
+          <h1>Our Campaigns</h1>
+        </div>
+      </main>
     </React.Fragment>
-  )
+  );
 }
 
-export default index
+export default index;
